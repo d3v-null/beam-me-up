54040d1b89cc6833ecb609d481a79634bf315942
d3v-null <dev.aleph.null@gmail.com>
2023-05-27 20:16:50 +0800

    🦅️ fly

--- src/engine/Camera.ts
+++ src/engine/Camera.ts
@@ -1,11 +1,12 @@
 import { Engine } from './Engine'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { FlyControls } from 'three/examples/jsm/controls/FlyControls.js'
 import { GameEntity } from './GameEntity'
 
 export class Camera implements GameEntity {
   public instance!: THREE.PerspectiveCamera
-  private controls!: OrbitControls
+  private controls!: OrbitControls | FlyControls
 
   constructor(private engine: Engine) {
     this.initCamera()
@@ -14,19 +15,31 @@ export class Camera implements GameEntity {
 
   private initCamera() {
     this.instance = new THREE.PerspectiveCamera(
-      75,
+      80,
       window.innerWidth / window.innerHeight,
       0.1,
-      1000
+      10000
     )
-    this.instance.position.z = 2
-    this.instance.position.y = 1
+    this.instance.position.x = 0
+    this.instance.position.y = 0
+    this.instance.position.z = 0
     this.engine.scene.add(this.instance)
   }
 
   private initControls() {
+    // this.controls = new FlyControls(this.instance, this.engine.canvas);
+    // this.controls.dragToLook = true;
+    // this.controls.autoForward = true;
+
     this.controls = new OrbitControls(this.instance, this.engine.canvas)
-    this.controls.autoRotate = true;
+    this.controls.keys.LEFT = 'KeyA';
+    this.controls.keys.RIGHT = 'KeyD';
+    this.controls.keys.BOTTOM = 'KeyS';
+    this.controls.keys.UP = 'KeyW';
+    this.controls.keyPanSpeed = 20;
+    this.controls.autoRotate = false;
+    this.controls.enableDamping = true;
+    this.controls.listenToKeyEvents(window);
     this.controls.update()
   }
 
@@ -35,7 +48,27 @@ export class Camera implements GameEntity {
     this.instance.updateProjectionMatrix()
   }
 
-  update() {
-    this.controls.update()
+  update(_delta: number) {
+    if (this.controls instanceof OrbitControls) {
+      this.controls.update()
+    } else {
+      this.controls.update(_delta)
+    }
+  }
+
+  setTarget(target: THREE.Vector3) {
+    if (this.controls instanceof OrbitControls) {
+      this.controls.target = target
+    }
+  }
+
+  translateCamera(delta: THREE.Vector3) {
+    this.instance.position.add(delta);
+  }
+
+  translateTarget(delta: THREE.Vector3) {
+    if (this.controls instanceof OrbitControls) {
+      this.controls.target.add(delta);
+    }
   }
 }
